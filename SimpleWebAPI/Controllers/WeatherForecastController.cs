e7b5745ee2e286008ca2c5cf40a0197d0a9e1bf5
dineshr <rdineshr@hotmail.com>
2021-01-24 16:10:17 -0700

    added summary

--- SimpleWebAPI/Controllers/WeatherForecastController.cs
+++ SimpleWebAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SimpleWebAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Breezy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
